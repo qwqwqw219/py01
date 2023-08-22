58742175f18d8bab9db7d32808c9180e477396f5
qwqwqw219 <KupriyanovVA@Nornik.ru>
2023-08-22 19:23:22 +0300

    комментарий

--- main04.py
+++ main04.py
@@ -4,8 +4,7 @@ def printBin( n ):
     k = 128
     print ( str(n) + "  :  ", end = "" )
     while k > 0:
-        print ( n // k, 
-                end = "" )
+        print ( n // k,  end = "" )
         n = n % k
         k = k // 2
-printBin ( a )
+printBin   ( a )
